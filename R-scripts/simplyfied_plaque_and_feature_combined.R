730e4064d144a31afdb5020d2db7eb4f62e4b66b
Luka Hoeben <101551767+LukaWill@users.noreply.github.com>
2023-07-04 12:54:33 +0200

    Add files via upload

--- R-scripts/simplyfied_plaque_and_feature_combined.R
+++ R-scripts/simplyfied_plaque_and_feature_combined.R
@@ -26,15 +26,15 @@ source("config.R")
 rawcounts <- read.table(counts_file, header = T, row.names = 1) #read rawcounts file
 
 #Normalization
-data <- DGEList(counts = rawcounts, genes = rownames(rawcounts)) #creates a DGEList object fro a table of counts
-data <- calcNormFactors(data, method = "TMM") #calculates normalization factors to scale of the raq library sizes
+data <- DGEList(counts = rawcounts, genes = rownames(rawcounts)) #Creates a DGEList object from a table of counts 
+data <- calcNormFactors(data, method = "TMM") #calculates normalization factors to scale of the raw library sizes
 counts <- cpm(data, log = FALSE) #counts per million
 counts.log <- cpm(data, log = TRUE) #counts per million, log2
-counts.log <- as.data.frame(counts.log) #set counts.log as data frame
-quantile.normalized.log =round(limma::normalizeQuantiles(counts.log)) #quantile normalization
+counts.log <- as.data.frame(counts.log) #sets counts.log as data.frame
+quantile.normalized.log =round(limma::normalizeQuantiles(counts.log)) #quantile normalization 
 
 #writing the different normalization steps to file
-write.table(counts, file = normalized_counts)
+write.table(counts, file = normalized_counts) 
 write.table(counts.log, file = normalized_counts_log)
 write.table(quantile.normalized.log, file = normalized_quantile)
 
@@ -49,9 +49,9 @@ dev.off()
 
 
 # Get the file names into one list with a for loop
-file_names <- list.files(directory_path, pattern = "^Genes_.*\\.txt$", full.names = TRUE) #looks for file names in selected directory_path
+file_names <- list.files(directory_path, pattern = "^Genes_.*\\.txt$", full.names = TRUE) #looks for filenames in selected directory_path
 networks <- list() #empty list
-for (i in 1:length(file_names)) { #checks all file_names and changes the name to network + number and adds it to the empty list.
+for (i in 1:length(file_names)) { #checks all file_names and changes the name to network + number and adds it to the empty list. 
   network_name <- gsub("^Genes_(.*)\\.txt$", "\\1", basename(file_names[i]))
   network_name <- paste0("network", network_name)
   network_object <- read.table(file_names[i], header = TRUE, sep = "\t")
@@ -59,7 +59,7 @@ for (i in 1:length(file_names)) { #checks all file_names and changes the name to
 }
 
 # Separate the dataframes in the networks list into individual dataframes
-list2env(networks, envir = .GlobalEnv)
+list2env(networks, envir = .GlobalEnv) 
 
 #read the counts file
 counts <- read.table(normalized_counts, header = T, row.names = 1)
@@ -109,7 +109,7 @@ result_table <- data.frame(Network = network_names_N,
 
 write.table(result_table, file = result_no_overlap, sep = "\t") #writes total to file
 
-#selects based on minimum of 25 genes and a maximum of 1000 genes and an overlap of at least 80% with the countsfile.
+#selects based on minimum of 25 genes and a maximum of 1000 genes and an overlap of at least 80% with the countsfile. 
 result_table <- filter(result_table, Total_Genes >25)
 result_table <- filter(result_table, Total_Genes <1000)
 result_table <- filter(result_table, Percentage_Overlap > 80)
@@ -119,35 +119,36 @@ column_name <- result_table$Network
 network_names_N <- result_table %>% pull(Network)
 network_names <- str_extract(column_name, "\\d+")
 
-#list with all networks
+#list with all nthe ensembl IDs from all selected networks
 Listnetworks <- list()
 for (name in network_names) {
   network_object <- get(paste0("network", name))
   Listnetworks[[name]] <- network_object$ensembl
 }
 
-
-#run module score
+#Module score
 module_score <- ScoreSignatures_UCell(counts, features = Listnetworks, maxRank = 7000) #calculates module score
 module_score <- as.data.frame(module_score) #set data frame
 module_score <- module_score %>% rownames_to_column(var = "study_number") #rownames to first columns 
 
-
-#adding the features to the module score 
-module_score_feature <- dplyr::full_join(features, module_score, by = "study_number") #join module score with feature
+cluster <- read.table(plaque_types, header = T) #add clusters (plaque types)
+module_score_feature <- dplyr::full_join(cluster, module_score, by = "study_number") #join module score and cluster
+module_score_feature <- dplyr::full_join(features, module_score_feature, by = "study_number") #join module score, cluster with features
 module_score_feature <- na.omit(module_score_feature) #remove all NAs
 module_score_feature <- module_score_feature[, -1] #deselect first column
-module_score_feature <- data.frame(module_score_feature) #set data.frame
+module_score_feature <- data.frame(module_score_feature) #set data frame
 names(module_score_feature)[1] <- "Feature" #rename first column
 
-#write to table
 write.table(module_score_feature, file = module_score_file, sep = "\t") #write to table
 
-#plotting
+module_score_feature <- arrange(module_score_feature, cluster) #arrange on cluster
+
+module_score_feature$cluster <- factor(module_score_feature$cluster, levels = unique(module_score_feature$cluster)) #set factor levels
 module_score_feature$Feature <- factor(module_score_feature$Feature, levels = unique(module_score_feature$Feature)) #set factor levels
-#set function for violin plot for module score
+
+#function violin plot for module score
 generate_plot <- function(column_index, title) {
-  ggplot(module_score_feature, aes(x = Feature, y = module_score_feature[, column_index], fill = Feature)) +
+  ggplot(module_score_feature, aes(x = cluster, y = module_score_feature[, column_index], fill = cluster)) +
     geom_violin() +
     geom_jitter(color = "black", size = 0.1, alpha = 0.9) +
     theme(
@@ -156,13 +157,14 @@ generate_plot <- function(column_index, title) {
       plot.title = element_text(size = 10),
       legend.text = element_text(size = 10), legend.title = element_text(size=10)
     ) +
-    labs(title = title, y = 'modulescore', x = 'Feature')
-} 
-
+    labs(title = title, y = 'modulescore', x = 'cluster') +
+    facet_grid(. ~Feature)
+}
 
 plots <- list() #empty list
+
 for (i in seq_along(network_names)) { #loops over all selected networks
-  column_index <- i + 1
+  column_index <- i + 2
   title <- network_names[i]
   
   plot <- list(column_index = column_index, title = title)
@@ -170,62 +172,93 @@ for (i in seq_along(network_names)) { #loops over all selected networks
   plots[[i]] <- plot
 }
 
-plots <- lapply(plots, function(plot) { #apply the function to all networks
+plots <- lapply(plots, function(plot) { #applys the function for plot
   generate_plot(plot$column_index, plot$title)
 })
 
 #plot 
 pl1 <- marrangeGrob(plots, nrow = 2, ncol = 2) #set parameters
-ggsave(violin_plot_module, pl1, width = 5, height = 5) #save plots and adds width / height parameters
-
+ggsave(violin_plot_module, pl1, width = 5, height = 5) #saves plots and adds width / height parameters
 
 
 #CONNECTIVITY
-
-#input counts
 counts_log <- read.table(normalized_counts_log, header = T, row.names = 1) #reads counts in log
 counts_log <- rownames_to_column(counts_log, "ensembl") #set rownames to column
 
-correlation_matrices <- list()#empty list
-unique_features <- unique(features[, 2])#selects the unique features
+correlation_matrices <- list() #empty list
+unique_features <- unique(features[, 2]) #selects the unique features
 unique_features <- na.omit(unique_features) #removes NAs
 unique_features <- as.character(unique_features) #set unique features as character
-correlations_list <- vector("list", length(unique_features))
-correlations_list <- list()
+unique_clusters <- unique(cluster[, 2]) #selects unique clusters. 
 
+correlations_list <- list() #empty list
+
+#loop for all selected networks
 for (name in network_names_N) {
   network <- get(name)
-  overlap <- dplyr::semi_join(counts_log, network, by = 'ensembl') #join counts with ensembl IDs network
+  overlap <- dplyr::semi_join(counts_log, network, by = "ensembl") #join counts with ensembl IDs network
   overlap <- column_to_rownames(overlap, var = "ensembl") #column to rownames
   overlap <- t(overlap) #transpose
-  overlap <- as.data.frame(overlap) #set dataframe
+  overlap <- as.data.frame(overlap) #set data frame
   overlap <- rownames_to_column(overlap, "study_number") #rownames to column
-  overlap_feature <- dplyr::full_join(features, overlap, by = "study_number") #join features with ensembl IDS 
-  overlap_feature <- arrange(overlap_feature, var = "study_number") #arrange on study_number
+  overlap_cluster <- dplyr::full_join(cluster, overlap, by = "study_number") #join the cluster with the ensembl IDS
+  overlap_cluster <- arrange(overlap_cluster, cluster) #arrange on cluster
+  overlap_feature <- full_join(features, overlap_cluster, by = "study_number") #join cluster including ensembl IDs with features
+  overlap_feature <- arrange(overlap_feature, cluster) #arrange on cluster
+  overlap_feature <- column_to_rownames(overlap_feature, var = 'study_number') #column to rownames
   overlap_feature <- na.omit(overlap_feature) #remove NAs
-  names(overlap_feature)[2] <- "Feature" #rename column 1
-  correlation <- cor(overlap_feature[, -(1:2)]) #calculate correlation for all ensembl IDs
-  correlation_matrices[[name]]<- correlation #adds the correlation per network to the empty list
+  names(overlap_feature)[1] <- "Feature" #change name column 1
+  correlation <- cor(overlap_feature[, -(1:2)]) #calculates correlation for all ensembl IDs
+  correlation_matrices[[name]]<- correlation #adds the correlation per network to the empty list 
+
+  #empty lists per cluster
+  filtered_features_0 <- list()
+  filtered_features_1 <- list()
+  filtered_features_2 <- list()
+  filtered_features_3 <- list()
+  filtered_features_4 <- list()
+  correlations_0 <- list()
+  correlations_1<- list()
+  correlations_2 <- list()
+  correlations_3 <- list()
+  correlations_4 <- list()
   
-  #empty lists
-  filtered_features <- list()
-  correlations <- list()
-  #loops over the unique features and filteres for each unique filter and then calculates the correlation
+  #loop that filters the genes on feature and on cluster and then calculates the correlation
   for ( feature in unique_features) {
-    filtered_features[[feature]] <- filter(overlap_feature, Feature == feature)
-    correlations[[feature]] <- cor(filtered_features[[feature]][, -(1:2)])
+    filtered_features_0[[feature]] <- filter(overlap_feature, Feature == feature, cluster == "0")
+    correlations_0[[feature]] <- cor(filtered_features_0[[feature]][, -(1:2)])
   }
   
-  correlations_list[[name]] <- correlations #adds all the calculated correlations to a list per network
+  for ( feature in unique_features) {
+    filtered_features_1[[feature]] <- filter(overlap_feature, Feature == feature, cluster == "1")
+    correlations_1[[feature]] <- cor(filtered_features_1[[feature]][, -(1:2)])
+  }
+  
+  for ( feature in unique_features) {
+    filtered_features_2[[feature]] <- filter(overlap_feature, Feature == feature, cluster == "2")
+    correlations_2[[feature]] <- cor(filtered_features_2[[feature]][, -(1:2)])
+  }
+  
+  for ( feature in unique_features) {
+    filtered_features_3[[feature]] <- filter(overlap_feature, Feature == feature, cluster == "3")
+    correlations_3[[feature]] <- cor(filtered_features_3[[feature]][, -(1:2)])
+  }
+  
+  for ( feature in unique_features) {
+    filtered_features_4[[feature]] <- filter(overlap_feature, Feature == feature, cluster == "4")
+    correlations_4[[feature]] <- cor(filtered_features_4[[feature]][, -(1:2)])
+  }
+  
+  #adds the calculated correlations to a list and rename the lists
+  correlations_list[[name]] <- list(correlations_0, correlations_1, correlations_2, correlations_3, correlations_4)
+  names(correlations_list[[name]]) <- c("0", "1", "2", "3", "4")
 }
-
-#correlation heatmap
+  
 pdf(file = heatmap_cor)
 # colorscheme and parameters 
 color.scheme <- rev(brewer.pal(10,"RdBu")) 
 par(mar = c(3, 3, 2, 2) + 0.1)
-
-cluster_genes <- list #empty list
+cluster_genes <- list() #empty list
 
 #loop to make a dendrogram from the correlation and makes a heatmap based on the correlation including the dendogram
 for(name in network_names_N) {
@@ -267,9 +300,9 @@ for(name in network_names_N) {
 dev.off()
 
 cluster_genes_total <- do.call(rbind, cluster_genes) #add all results in the list with dendogram to a dataframe
-cluster_genes_total <- rownames_to_column(cluster_genes_total, var = "network" )#rownames to column
+cluster_genes_total <- rownames_to_column(cluster_genes_total, var = "network" ) #rownames to column
 cluster_genes_total <- cluster_genes_total[, -1] #removes column 1
-num_columns <- ncol(cluster_genes_total)#checks amount of columns
+num_columns <- ncol(cluster_genes_total) #checks amount of columns
 
 #gives header to data frame
 if(num_columns == 2) {
@@ -284,16 +317,14 @@ if(num_columns == 2) {
 write.table(cluster_genes_total, file = dendogram_cluster_genes, sep = "\t")
 
 
-#connectivity based on average correlation
-
-# function to calculate the average correlation  #NEEDS TO BE CHECKED FOR THE PLAQUES
+#function to calculate the average correlation
 calculate_average_correlation_r <- function(correlation_matrix) {
-  correlation_r <- as.data.frame(correlation_matrix) #set dataframe
-  correlation_r_long  <- gather(correlation_r) #matrix to long dataframe
-  correlation_r_long <- correlation_r_long %>% filter
+  correlation_r <- as.data.frame(correlation_matrix) #set data frame
+  correlation_r_long  <- gather(correlation_r) # matrix to long data frame
+  correlation_r_long <- correlation_r_long %>% filter 
   correlation_r_long$value <- abs(correlation_r_long$value) #absolute value
-  average_correlation_r <- correlation_r_long %>% group_by(key) %>% summarize(average_correlation_R = mean(value)) #calculates average of the absolute value
-  average_correlation_r <- as.data.frame(average_correlation_r) #sets data frame
+  average_correlation_r <- correlation_r_long %>% group_by(key) %>% summarize(average_correlation_R = mean(value)) #average of the absolute value
+  average_correlation_r <- as.data.frame(average_correlation_r) #set data frame
   colnames(average_correlation_r) <- c("genes", "average_correlation") #add column names
   return(average_correlation_r)
 }
@@ -304,31 +335,35 @@ average_correlation_total <- list() #empty list
 for (name in network_names_N) {
   network <- get(name)
   # Calculate average correlation for correlation_matrices
+  
   for (feature in unique_features) {
     average_correlation <- calculate_average_correlation_r(correlations_list[[name]]) #calculate average correlation for all networks for all features per cluster
     average_correlations <- as.character(average_correlation$genes) #sets genes as character
     #splits the first column on the dot and gives 3 new columns
-    split_values <- strsplit(average_correlations, "\\.")
+    split_values <- strsplit(average_correlations, "\\.") 
     before_dot <- sapply(split_values, `[`, 1)
-    after_dot <- sapply(split_values, `[`, 2)
-    average_correlation <- data.frame(feature = before_dot, genes= after_dot, average_correlation = average_correlation[, 2])
-    colnames(average_correlation) <- c("feature", "genes", "Correlation")
+    inbetween_dots <- sapply(split_values, `[`, 2)
+    after_dot <- sapply(split_values, `[`, 3)
+    average_correlation <- data.frame(cluster = before_dot, feature = inbetween_dots, genes= after_dot, average_correlation = average_correlation[, 2])
+    colnames(average_correlation) <- c("cluster", "feature", "genes", "Correlation")
+    
     #removes an X that is generated within the clustername
-    if (any(grepl("X", average_correlation$feature))) {
+    if (any(grepl("X", average_correlation$cluster))) {
       # Remove 'X' if it appears in column 1
-      average_correlation$feature <- gsub("X", "", average_correlation$feature)
+      average_correlation$cluster <- gsub("X", "", average_correlation$cluster)
     }
   }
-  average_correlation_total[[name]] <- average_correlation[, -2]#adds all the average correlation (connectivity) per network to one list
+  average_correlation_total[[name]] <- average_correlation[, -3] #adds all the average correlation (connectivity) per network to one list
 }
 
-plot_list <- list() #empty lists
-#loops over all selected networks and generates a plot  
+plot_list <- list() #empty list
+
+#loops over all selected networks and generates a plot
 for (name in network_names_N) {
   network <- get(name)
   average_correlations <- average_correlation_total[[name]]
   
-  p <- ggplot(average_correlations, aes(x = feature, y = Correlation, fill = feature)) +
+  p <- ggplot(average_correlations, aes(x = cluster, y = Correlation, fill = cluster)) +
     geom_violin() +
     geom_jitter(color = "black", size = 0.1, alpha = 0.9) +
     theme(
@@ -337,7 +372,8 @@ for (name in network_names_N) {
       plot.title = element_text(size = 10),
       legend.text = element_text(size = 10), legend.title = element_text(size=10)
     ) +
-    labs(title = name, y = "Average correlation", x = 'feature')
+    labs(title = name, y = "Average correlation", x = 'cluster') +
+    facet_grid(. ~feature)
   
   plot_list[[name]] <- p
 }   
@@ -345,41 +381,7 @@ for (name in network_names_N) {
 ml <- marrangeGrob(plot_list, nrow = 2, ncol = 2) #set parameters
 ggsave(connectivity_plot, ml, width = 5, height = 5) #save plots and add extra parameters
 
+
 save(average_correlation_total, file = average_correlation_file) #saves average correlation list to RData file
 
-
-
-#Statistics 
 source("ANOVA_automatic.R") #runs ANOVA for both module score and connectivity
-
-
-#separates the networks in different clusters based on the dendogram
-
-ensembl_ids_a <- list()
-ensembl_ids_b <- list()
-
-dir.create(directory)
-
-for (name in network_names) {
-  filtered_cluster_genes <- filter(cluster_genes_total, network == name)
-  cluster_1 <- filter(filtered_cluster_genes, dendogram_cluster == "cluster_1")
-  cluster_2 <- filter(filtered_cluster_genes, dendogram_cluster == "cluster_2")
-
-  cluster_1 <- cluster_1$ensembl
-  cluster_2 <- cluster_2$ensembl
-  extra_character <- "ensembl"
-
-  ensembl_ids_a[[name]] <- c(extra_character, cluster_1)
-  ensembl_ids_b[[name]] <- c(extra_character, cluster_2)
-
-  for (i in seq_along(ensembl_ids_a)) {
-    file_name_a <- file.path(directory, paste0("Genes_", name, "a", ".txt"))
-    writeLines(ensembl_ids_a[[i]], file_name_a)
-  }
-  for (i in seq_along(ensembl_ids_b)) { 
-    file_name_b <- file.path(directory, paste0("Genes_", name, "b", ".txt"))
-    writeLines(ensembl_ids_b[[i]], file_name_b)
-  }
-}
-
-
